f679f77ac8f4df2404afe7fdc9ef861a7f5529d8
JaeJunday <wowns8114@gmail.com>
2023-09-08 13:41:29 +0900

    chore: axios-auth-refresh dependency

--- services/react/init.sh
+++ services/react/init.sh
@@ -1,8 +1,10 @@
 echo "----- Start installing npm dependencies -----"
 
-# npm install --prefix srcs react
+npm install --prefix srcs react
 # npm install --prefix srcs --save react-unity-webgl
 # npm install --prefix srcs --save axios
+# npm install --prefix srcs --save  axios-auth-refresh
+# npm install --prefix srcs --save react-router-dom
 
 echo "----- Successfully installed -----"
 
