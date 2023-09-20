52f424a2dc0c46a4e1d0da0e681cddf8ea2294b3
Jaejun Kim <jaejkim@c4r4s4.42seoul.kr>
2023-09-20 14:03:41 +0900

    feat: image upload preview add

--- services/next/init.sh
+++ services/next/init.sh
@@ -8,7 +8,7 @@ npm install --prefix srcs --save @headlessui/react
 # npm install --prefix srcs --save axios axios-auth-refresh
 # npm install --prefix srcs --save @typescript-eslint
 
-npm update --prefix srcs next @headlessui/react redux react-redux @reduxjs/toolkit react-devtools react-router-dom react-cookie jsonwebtoken react-unity-webgl nextjs-unity-webgl axios axios-auth-refresh @typescript-eslint turbo
+# npm update --prefix srcs next @headlessui/react redux react-redux @reduxjs/toolkit react-devtools react-router-dom react-cookie jsonwebtoken react-unity-webgl nextjs-unity-webgl axios axios-auth-refresh @typescript-eslint turbo
 echo "----- Successfully installed -----"
 
 npm --prefix srcs run dev --cache-clear
--- srcs/next/src/app/register/ImageForm.tsx
+++ srcs/next/src/app/register/ImageForm.tsx
@@ -1,21 +1,59 @@
 'use client';
-import React from 'react';
-import { useDispatch, Provider } from 'react-redux';
-import { RootState } from '../redux/store';
+import React, { useState, ChangeEvent, useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux';
 import { setImageUrl } from '../redux/userSlice';
 import store from '../redux/store';
 
 const ImageFormContent = () => {
     const dispatch = useDispatch();
+    const [previewImage, setPreviewImage] = useState<string | null>(null);
 
-    const upload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value);
-        dispatch(setImageUrl(event.target.value));
+    const upload = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            const file = event.target.files[0];
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                if (e.target) {
+                    const img = new Image();
+                    img.src = e.target.result as string;
+                    img.onload = () => {
+                        const canvas = document.createElement('canvas');
+                        const ctx = canvas.getContext('2d')!;
+                        const size = 500; // 정사각형 크기
+
+                        canvas.width = size;
+                        canvas.height = size;
+
+                        // 이미지를 비율 무시하고 정사각형으로 조정
+                        ctx.drawImage(img, 0, 0, size, size);
+
+                        const resizedImageUrl = canvas.toDataURL('image/jpeg'); // 또는 'image/png' 등으로 변경 가능
+                        // console.log('resizedImageUrl: ', resizedImageUrl);
+                        setPreviewImage(resizedImageUrl);
+                        dispatch(setImageUrl(resizedImageUrl));
+                    };
+                }
+            };
+
+            reader.readAsDataURL(file);
+        }
     };
+
+    useEffect(() => {
+        // 초기 이미지 설정
+        setPreviewImage('');
+    }, []);
 
     return (
         <form id="imageForm" encType="multipart/form-data">
-            <label htmlFor="imageUpload"> Upload image (500x500): </label>
+            <label htmlFor="imageUpload">Upload image (180x180):</label>
+            <img
+                src={previewImage || ''}
+                alt="Preview"
+                style={{ maxWidth: '180px', maxHeight: '180px' }}
+            />
+            <br />
             <input
                 type="file"
                 id="imageUpload"
